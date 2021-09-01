5a7998edd272067b283a3d9f10222f4f4f3c3391
Maksim Zakharenkov <maxxzahar@rambler.ru>
2021-09-01 15:58:44 +0300

    Listeners are attached

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-const c1 = document.getElementById('1cbid');
-c1.addEventListener('click', function(){
-    console.log(c1.id.slice(0, -3));
-    const template = document.getElementById(c1.id.slice(0, -3));
-    c1.parentNode.insertAdjacentElement('afterend', template.content.firstElementChild.cloneNode(true));
-    c1.addEventListener('click', function(){
-    
-    });
-}, {once: true});
+// const c1 = document.getElementById('1cbid');
+// c1.addEventListener('click', function(){openList(c1.id)}, {once: true});
+const wrapper = document.querySelector('.wrapper');
+addListeners(wrapper);
+
+function openList(id){
+    console.log('open', id);
+    const template = document.getElementById(id.slice(0, -3));
+    const element = document.getElementById(id);
+    element.parentNode.insertAdjacentElement('afterend', template.content.firstElementChild.cloneNode(true));
+    element.addEventListener('click', function(){closeList(id)}, {once: true});
+}
+
+function closeList(id){
+    console.log('close', id);
+    const element = document.getElementById(id);
+    element.parentNode.nextSibling.remove();
+    element.addEventListener('click', function(){openList(id)}, {once: true});
+}
+
+function addListeners(parent){
+    const list = parent.querySelector('.card');
+    const items = list.querySelectorAll('li');
+    for (let i = 0; i < items.length; i++){
+        if (!items[i].classList.contains('last')){
+            items[i].firstElementChild.addEventListener('click', function(){openList(items[i].firstElementChild.id)}, {once: true});
+        }
+    }
+}
